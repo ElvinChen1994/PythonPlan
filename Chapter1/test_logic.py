d759db0c0c6ca0a5e76f9b23520adbe95a3c3331
chenzhengyang <chenzhengyang@tianchimedia.com>
2021-07-26 18:44:20 +0800

    简单算法

---
+++ Chapter1/test_logic.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# @Time: 2021/7/26 3:34 下午
+# @Author: Elvin
+'''水仙花数'''
+# for num in range(100, 1000):
+#     low = num % 10
+#     mid = num // 10 % 10
+#     high = num // 100
+#     if num == low ** 3 + mid ** 3 + high ** 3:
+#         print(num)
+
+'''正整数反转'''
+num = int(input("num = "))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
+
+'''穷举法'''
+for x in  range(0,10):
+    for y in range(0, 20):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z / 3 == 100:
+            print('公鸡：%d, 母鸡：%d, 小鸡：%d' % (x, y, z))
+
